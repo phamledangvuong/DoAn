67166deabdb3e55ca21dd609c17d2cbca95dcf8a
phamledangvuong <vuongpham190506@gmail.com>
2026-05-11 16:53:36 +0700

    init project

---
+++ TaoMoi/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace phamledangvuong.TaoMoi
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("Data Source=.;Initial Catalog=QuanLySanPham;Integrated Security=True")
+        {
+        }
+
+        public DbSet<DanhMuc> DanhMucs { get; set; }
+    }
+}
---
+++ ViewModel/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phamledangvuong.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
